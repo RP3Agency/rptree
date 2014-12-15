46e673ddb57e991062ac1a177c40a83a53424a71
Bryan Cox <bcox@rp3agency.com>
2014-12-15 14:32:18 -0500

    More debugging logs on Snowball local TCP client; adding half a second to Menorah reset

--- localclient/index.js
+++ localclient/index.js
@@ -6,18 +6,26 @@ var config 		= require('config'),
 		console.log('Connected to Snowball.');		//an homage to RPTree 1.0.  Long Live Snowball.
 	});
 
-	//For now, whenever we recieve data, start the sequencer
-	tcpClient.on('data', function () {
-		oscClient.send('/sequencer/start 1');
-		oscClient.send('/menorah/flash');
-		setTimeout(function () {
-			oscClient.send('/menorah/reset');
-		}, 1000);
-	});
+//For now, whenever we recieve data, start the sequencer
+tcpClient.on('data', function (data) {
+	console.log('Incoming: ', data.toString());
 
-	tcpClient.on('end', function() {
-	  console.log('Disconnected from Snowball.');
-	});
+	console.log('Throwing Snowball at Tree.');
+	oscClient.send('/sequencer/start 1');
 
+	console.log('Throwing Snowball at Menorah.');
+	oscClient.send('/menorah/flash');
 
+	setTimeout(function () {
+		console.log('Relighting Menorah.');
+		oscClient.send('/menorah/reset');
+	}, 1500);
+});
 
+tcpClient.on('end', function() {
+	console.log('Disconnected from Snowball.');
+});
+
+tcpClient.on('error', function(err) {
+	console.log('Snowball error! ', err);
+});

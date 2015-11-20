35d1add51497ef6d23084f7dc4e5aff7eb2fe2a9
Tracy Rotton <trotton@rp3agency.com>
2015-11-20 13:53:49 -0500

    [#74] commented out everything from last year, as much of it may need to be rewritten

--- src/js/rptree.backbone.js
+++ src/js/rptree.backbone.js
@@ -7,159 +7,159 @@ if ( rptree === undefined ) { var rptree = {}; }
  * rptree.backbone
  * Backbone all the things!
  */
-rptree.backbone = (function($, _, Backbone) {
+// rptree.backbone = (function($, _, Backbone) {
 
-	'use strict';
+// 	'use strict';
 
-	// Determine if we're on the microsite or the screen page
+// 	// Determine if we're on the microsite or the screen page
 
-	var microsite, screen;
+// 	var microsite, screen;
 
-	if ( $('html').hasClass('screen') ) {
-		microsite = false;
-		screen = true;
-	} else {
-		microsite = true;
-		screen = false;
-	}
+// 	if ( $('html').hasClass('screen') ) {
+// 		microsite = false;
+// 		screen = true;
+// 	} else {
+// 		microsite = true;
+// 		screen = false;
+// 	}
 
-	// Cache the tweet template for reuse
-	var tweetTemplate = _.template( $("#tweet-template").html() ),
+// 	// Cache the tweet template for reuse
+// 	var tweetTemplate = _.template( $("#tweet-template").html() ),
 
-	/**
-	 * Backbone Classes
-	 */
+// 	/**
+// 	 * Backbone Classes
+// 	 */
 
-	/** Tweet model */
-	TweetModel = Backbone.Model.extend(),
+// 	/** Tweet model */
+// 	TweetModel = Backbone.Model.extend(),
 
-	/** Tweets Collection */
-	TweetsCollection = Backbone.Collection.extend({
-		isUpdating: false,
-		firstTweetID: null,
-		lastTweetID: null,
-		url: function() {
-			var url = 'http://' + location.host + '/feed';
-			var params = this.isUpdating ? { since: this.firstTweetID } : { priorTo: this.lastTweetID };
-			if(screen) {
-				params.limit = 1;
-			}
-			if(_.any(params, _.identity)) {
-				url += '?' + $.param(_.pick(params, _.identity));
-			}
-			return url;
-		},
-		model: TweetModel
-	}),
+// 	/** Tweets Collection */
+// 	TweetsCollection = Backbone.Collection.extend({
+// 		isUpdating: false,
+// 		firstTweetID: null,
+// 		lastTweetID: null,
+// 		url: function() {
+// 			var url = 'http://' + location.host + '/feed';
+// 			var params = this.isUpdating ? { since: this.firstTweetID } : { priorTo: this.lastTweetID };
+// 			if(screen) {
+// 				params.limit = 1;
+// 			}
+// 			if(_.any(params, _.identity)) {
+// 				url += '?' + $.param(_.pick(params, _.identity));
+// 			}
+// 			return url;
+// 		},
+// 		model: TweetModel
+// 	}),
 
-	/** TweetListView */
-	TweetListView = Backbone.View.extend({
-		el: "#page__tweets",
-		initialize: function() {
-			_.bindAll(this, 'checkScroll', 'refresh');
-			if (microsite) {
-				$(window).scroll(this.checkScroll);
-			}
-			this.timer = setInterval(this.refresh, 5000);
-			this.isLoading = false;
-			this.isAppending = false;
-			this.tweetsCollection = new TweetsCollection();
-			this.render();
-		},
-		render: function() {
-			this.loadResults();
-		},
-		refresh: function() {
-			this.loadResults(true);
-		},
-		loadResults: function(refresh) {
-			var that = this;
+// 	/** TweetListView */
+// 	TweetListView = Backbone.View.extend({
+// 		el: "#page__tweets",
+// 		initialize: function() {
+// 			_.bindAll(this, 'checkScroll', 'refresh');
+// 			if (microsite) {
+// 				$(window).scroll(this.checkScroll);
+// 			}
+// 			this.timer = setInterval(this.refresh, 5000);
+// 			this.isLoading = false;
+// 			this.isAppending = false;
+// 			this.tweetsCollection = new TweetsCollection();
+// 			this.render();
+// 		},
+// 		render: function() {
+// 			this.loadResults();
+// 		},
+// 		refresh: function() {
+// 			this.loadResults(true);
+// 		},
+// 		loadResults: function(refresh) {
+// 			var that = this;
 
-			if(this.isLoading) {
-				return this;
-			}
-			this.isLoading = true;
-			if(refresh) {
-				this.tweetsCollection.isUpdating = true;
-			}
-			this.tweetsCollection.fetch({
-				success: function(tweets) {
-					var content = $( tweetTemplate({ tweets: tweets.models }) );
+// 			if(this.isLoading) {
+// 				return this;
+// 			}
+// 			this.isLoading = true;
+// 			if(refresh) {
+// 				this.tweetsCollection.isUpdating = true;
+// 			}
+// 			this.tweetsCollection.fetch({
+// 				success: function(tweets) {
+// 					var content = $( tweetTemplate({ tweets: tweets.models }) );
 
-					if ( screen && tweets.models.length) {
-						that.$el.html(content);
-					}
+// 					if ( screen && tweets.models.length) {
+// 						that.$el.html(content);
+// 					}
 
-					if ( microsite ) {
-						if(refresh) {
-							content.insertBefore($('.tweet:first()', that.$el));
-							that.$el.masonry('prepended', content);
-						} else {
-							that.$el.append(content);
-							if(that.isAppending) {
-								that.$el.masonry('appended', content);
-							} else {
-								that.$el.masonry({
-									columnWidth: '.tweet',
-									itemSelector: '.tweet',
-									gutter: '.masonry-gutter',
-									stamp: ".page__video"
-								});
-								that.isAppending = true;
-							}
-						}
-						that.$el.imagesLoaded(function() {
-							that.$el.masonry();
-						});
-						content.find('.tweet__timestamp').prettyDate();
-					}
+// 					if ( microsite ) {
+// 						if(refresh) {
+// 							content.insertBefore($('.tweet:first()', that.$el));
+// 							that.$el.masonry('prepended', content);
+// 						} else {
+// 							that.$el.append(content);
+// 							if(that.isAppending) {
+// 								that.$el.masonry('appended', content);
+// 							} else {
+// 								that.$el.masonry({
+// 									columnWidth: '.tweet',
+// 									itemSelector: '.tweet',
+// 									gutter: '.masonry-gutter',
+// 									stamp: ".page__video"
+// 								});
+// 								that.isAppending = true;
+// 							}
+// 						}
+// 						that.$el.imagesLoaded(function() {
+// 							that.$el.masonry();
+// 						});
+// 						content.find('.tweet__timestamp').prettyDate();
+// 					}
 
-					var those = that.tweetsCollection;
-					if(tweets.models.length) {
-						if(those.firstTweetID === null || tweets.first().get('id') > those.firstTweetID) {
-							those.firstTweetID = tweets.first().get('id');
-						}
-						if(those.lastTweetID === null || tweets.last().get('id') < those.lastTweetID) {
-							those.lastTweetID = tweets.last().get('id');
-						}
-					}
-					those.isUpdating = false;
-					that.isLoading = false;
-				},
-				error: function() {
-					// silently return to listening state
-					that.tweetsCollection.isUpdating = false;
-					that.isLoading = false;
-				}
-			});
+// 					var those = that.tweetsCollection;
+// 					if(tweets.models.length) {
+// 						if(those.firstTweetID === null || tweets.first().get('id') > those.firstTweetID) {
+// 							those.firstTweetID = tweets.first().get('id');
+// 						}
+// 						if(those.lastTweetID === null || tweets.last().get('id') < those.lastTweetID) {
+// 							those.lastTweetID = tweets.last().get('id');
+// 						}
+// 					}
+// 					those.isUpdating = false;
+// 					that.isLoading = false;
+// 				},
+// 				error: function() {
+// 					// silently return to listening state
+// 					that.tweetsCollection.isUpdating = false;
+// 					that.isLoading = false;
+// 				}
+// 			});
 
-			return this;
-		},
-		events: {
-			'scroll': 'checkScroll'
-		},
-		checkScroll: _.debounce(function() {
-			if( !this.isLoading && ($(window).scrollTop() + $(window).height() > $(document).height() - 100) ) {
-				this.loadResults();
-			}
-		}, 100)
-	}),
+// 			return this;
+// 		},
+// 		events: {
+// 			'scroll': 'checkScroll'
+// 		},
+// 		checkScroll: _.debounce(function() {
+// 			if( !this.isLoading && ($(window).scrollTop() + $(window).height() > $(document).height() - 100) ) {
+// 				this.loadResults();
+// 			}
+// 		}, 100)
+// 	}),
 
-	/**
-	 * Backbone Objects
-	 */
+// 	/**
+// 	 * Backbone Objects
+// 	 */
 
-	init = function() {
-	};
+// 	init = function() {
+// 	};
 
-	return {
-		init:init,
-		tweetListView: new TweetListView()
-	};
+// 	return {
+// 		init:init,
+// 		tweetListView: new TweetListView()
+// 	};
 
-}(jQuery, _, Backbone));
+// }(jQuery, _, Backbone));
 
-(function() {
-	'use strict';
-	rptree.backbone.init();
-}());
+// (function() {
+// 	'use strict';
+// 	rptree.backbone.init();
+// }());

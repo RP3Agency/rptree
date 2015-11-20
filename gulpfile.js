8e5da0611821338ca4d83a7faff23eb3deec769a
Tracy Rotton <trotton@rp3agency.com>
2015-11-20 13:19:22 -0500

    [#74] commented out everything that was returning an error for now

--- gulpfile.js
+++ gulpfile.js
@@ -184,10 +184,11 @@ gulp.task( 'scripts-bower', function() {
 	var bower_dir = __dirname + '/bower_components',
 		bower_files = [
 			bower_dir + '/jquery/dist/jquery.js',
-			bower_dir + '/better-dom/dist/better-dom.js',
-			bower_dir + '/better-prettydate/dist/better-prettydate.js',
-			bower_dir + '/underscore/underscore.js',
-			bower_dir + '/masonry/dist/masonry.pkgd.js'
+			// bower_dir + '/better-dom/dist/better-dom.js',
+			// bower_dir + '/better-prettydate/dist/better-prettydate.js',
+			// bower_dir + '/underscore/underscore.js',
+			// bower_dir + '/backbone/backbone.js',
+			// bower_dir + '/masonry/dist/masonry.pkgd.js'
 		];
 
 	return gulp.src( bower_files )

715263aeaceb1e330d223065463213bfb12fe35c
Jeremy R. Hilts <jhilts@rp3agency.com>
2015-12-07 12:02:07 -0500

    fixed committed too-high-level

--- src/static/js/rpyeti.game.js
+++ src/static/js/rpyeti.game.js
@@ -111,7 +111,7 @@ RPYeti.game = (function() {
 			$(window).on('resize', this.resize);
 			setTimeout(this.resize, 1);
 
-			setTimeout(function () { self.gameplay.start(100, true); }, 500)
+			setTimeout(function () { self.gameplay.start(0, true); }, 1000)
 		},
 
 		/** Methods / Callbacks **/
